d04a0de4581d6532f50a927c8a524f8e98efcd0d
Toru Matsuoka <lettas0726@gmail.com>
2016-09-01 13:24:20 +0900

    threeをnpmでインストール

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('babel_build', function() {
-  return gulp.src('js/mnpk-sphere.js')
+  return gulp.src('js/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('babel_watch', function() {
-  gulp.watch('js/mnpk-sphere.js', ['babel_build']);
+  gulp.watch('js/*.js', ['babel_build']);
 });
 
 gulp.task('default', ['babel_build', 'babel_watch']);
