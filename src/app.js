7937e33e76908c0e2f3e5acbbedaec12809ef208
Toru Matsuoka <lettas0726@gmail.com>
2016-09-01 14:50:25 +0900

    sphere image canvasを別モジュールに

--- src/app.js
+++ src/app.js
@@ -1,51 +1,31 @@
 import * as THREE from 'three';
+import SphereImageCanvas from './sphere-image-canvas';
 import './control/OrbitControls';
 
-function createCanvas(sphereImage) {
-  var url = sphereImage.dataset.url;
-  var width = sphereImage.dataset.width;
-  var height = sphereImage.dataset.height;
+function initialize() {
+  const canvases = [];
 
-  var scene = new THREE.Scene();
-
-  var geometry = new THREE.SphereGeometry(5, 60, 40);
-  geometry.scale(-1, 1, 1);
-
-  var textureLoader = new THREE.TextureLoader();
-  var material = new THREE.MeshBasicMaterial({ map: textureLoader.load(url) });
-
-  var sphere = new THREE.Mesh(geometry, material);
-  scene.add(sphere);
-
-  var camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
-  camera.position.set(0,0,0.1);
-  camera.lookAt(sphere.position);
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(width, height);
-  renderer.setClearColor({color: 0x000000});
-  renderer.render(scene, camera);
-
-  var controls = new THREE.OrbitControls(camera, renderer.domElement);
+  const imageContainers = document.querySelectorAll('.mnpk-sphere-image');
+  for (const imageContainer of imageContainers) {
+    const { url, width, height } = imageContainer.dataset;
+    const canvas = new SphereImageCanvas(url, width, height);
+    imageContainer.appendChild(canvas.domElement);
+    canvases.push(canvas);
+  }
 
   function render() {
     window.requestAnimationFrame(render);
-    sphere.rotation.y += 0.05 * Math.PI / 180;
-    renderer.render(scene, camera);
-    controls.update();
+
+    for (const canvas of canvases) {
+      canvas.update();
+    }
+  }
+
+  for (const canvas of canvases) {
+    console.log(canvas);
   }
 
   render();
-
-  return renderer.domElement;
-}
-
-function initialize() {
-  var sphereImages = document.querySelectorAll('.mnpk-sphere-image');
-  for (var i = 0; i < sphereImages.length; i++) {
-    var canvas = createCanvas(sphereImages[i]);
-    sphereImages[i].appendChild(canvas);
-  }
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
---
+++ src/sphere-image-canvas.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+
+const textureLoader = new THREE.TextureLoader();
+
+export default class SphereImageCanvas {
+  constructor(url, width, height) {
+    const scene = new THREE.Scene();
+
+    const sphere = this._createSphereMesh(url);
+    scene.add(sphere);
+
+    const camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
+    camera.position.set(0,0,0.1);
+    camera.lookAt(sphere.position);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    renderer.setClearColor({ color: 0x000000 });
+
+    this.url = url;
+    this.width = width;
+    this.height = height;
+    this.scene = scene;
+    this.sphere = sphere;
+    this.camera = camera;
+    this.renderer = renderer;
+    this.domElement = renderer.domElement;
+  }
+
+  update() {
+    this.sphere.rotation.y += 0.05 * Math.PI / 180;
+    this.renderer.render(this.scene, this.camera);
+    //this.controls.update();
+  }
+
+  _createSphereMesh(url) {
+    const geometry = new THREE.SphereGeometry(5, 60, 40);
+    geometry.scale(-1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ map: textureLoader.load(url) });
+    return new THREE.Mesh(geometry, material);
+  }
+}
+
