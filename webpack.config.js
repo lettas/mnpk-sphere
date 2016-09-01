a9b5f7684242ed9fb22eca0c581c9b7490a8ff39
Toru Matsuoka <lettas0726@gmail.com>
2016-09-01 14:26:05 +0900

    3rd party ライブラリのwarningを個別に消す方法はないらしいので全部OFF

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   devtool: '#source-map',
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
   ],
   module: {
     loaders: [
